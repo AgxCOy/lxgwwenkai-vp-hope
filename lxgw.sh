42dd2a3337aa40bcf82442242fbced212c40a4cf
SilverAg.L <caclx@outlook.com>
2025-07-16 15:13:29 +0800

    no need minify again :)

--- lxgw.sh
+++ lxgw.sh
@@ -9,15 +9,13 @@ pnpm -v
 pnpm i
 pnpm build
 pnpm start
-
 cp ./src/package.json.template ./private/package.json
-pnpx css-minify -d /private
 
 PKG_TOKEN=8614a238a97249223cedc574b13c6b2c7c325abc
 pnpm config set @agxcoy:registry https://git.liteyuki.org/api/packages/AgxCOy/npm
 pnpm config set -- '//git.liteyuki.org/api/packages/AgxCOy/npm/:_authToken' "$PKG_TOKEN"
 
-cd /private
+cd ./private
 NVER=$(cat ./VERSION | sed 's/^v//' | awk -F. '{if($3==null) print$1"."$2".0"; else print$0}')
 pnpm version $NVER --no-git-tag-version
 pnpm adduser --registry https://git.liteyuki.org/api/packages/AgxCOy/npm --scope=@agxcoy --auth-type=legacy
